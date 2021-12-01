c718f1f35fea2d615c571205a5876b72237ddb24
Jack Lyons <jack.edward.lyons@gmail.com>
2021-12-01 15:22:03 +1000

    Feat: Add restaurants controller and DAO

---
+++ backend/api/dao/restaurantsDAO.js
@@ -0,0 +1,65 @@
+let restaurants;
+
+/**
+ * In computer software, a data access object (DAO) is a pattern
+ * that provides an abstract interface to some type of database
+ * or other persistence mechanism.
+ */
+export default class RestaurantsDAO {
+  static async injectDB(conn) {
+    if (restaurants) {
+      return;
+    }
+    try {
+      restaurants = await conn
+        .db(process.env.RESTREVIEWS_NS)
+        .collection("restaurants");
+    } catch (e) {
+      console.error(
+        `Unable to establish a collection handle in restaurantsDAO: ${e}`
+      );
+    }
+  }
+
+  static async getRestaurants({
+    filters = null,
+    page = 0,
+    restaurantsPerPage = 20,
+  } = {}) {
+    let query;
+    if (filters) {
+      if ("name" in filters) {
+        query = { $text: { $search: filters["name"] } };
+      } else if ("cuisine" in filters) {
+        query = { cuisine: { $eq: filters["cuisine"] } };
+      } else if ("zipcode" in filters) {
+        query = { "address.zipcode": { $eq: filters["zipcode"] } };
+      }
+    }
+
+    let cursor;
+
+    try {
+      cursor = await restaurants.find(query);
+    } catch (e) {
+      console.error(`Unable to issue find command, ${e}`);
+      return { restaurantsList: [], totalNumRestaurants: 0 };
+    }
+
+    const displayCursor = cursor
+      .limit(restaurantsPerPage)
+      .skip(restaurantsPerPage * page);
+
+    try {
+      const restaurantsList = await displayCursor.toArray();
+      const totalNumRestaurants = await restaurants.countDocuments(query);
+
+      return { restaurantsList, totalNumRestaurants };
+    } catch (e) {
+      console.error(
+        `Unable to convert cursor to array or problem counting documents, ${e}`
+      );
+      return { restaurantsList: [], totalNumRestaurants: 0 };
+    }
+  }
+}
